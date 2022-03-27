48a0fd5b637186b0ccbe9e193219ba7d619346dd
Marcelo Shinji Shinzato <43659775+MSShinzato@users.noreply.github.com>
2022-03-27 20:18:01 -0300

    Fix Is#11

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -2,8 +2,8 @@ package br.calebe.ticketmachine.core;
 
 import java.util.Iterator;
 
-public class Troco1{
-    public Troco1(int valor) {
+public class Troco2 {
+    public Troco2(int valor) {
         PapelMoeda[] papeisMoeda;
         int novoValor;
         int valorOriginal=valor;
@@ -27,8 +27,12 @@ public class Troco1{
         }
         papeisMoeda[4] = new PapelMoeda(50, count);
         count = 0;
-        while (valor % 20 != 0) {
+        while (true) {
+            novoValor=novoValor-20;
             count++;
+            if(novoValor<20){
+                break;
+            }
         }
         papeisMoeda[3] = new PapelMoeda(20, count);
         count = 0;
