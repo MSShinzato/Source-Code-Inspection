b3a318461381c3c6346ff353d6dccbbdad08b168
Kaike <kzuanetti53@gmail.com>
2022-04-01 22:30:38 -0300

    add getQuantidade test

--- Source Code Inspection/src/br/calebe/ticketmachine/test/core/PapelMoedaTeste.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/test/core/PapelMoedaTeste.java
@@ -1,15 +1,22 @@
 package br.calebe.ticketmachine.test.core;
 
-import br.calebe.ticketmachine.core.*;
+import br.calebe.ticketmachine.core.PapelMoeda;
 
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class PapelMoedaTeste {
+    
+    
     @Test
     public void getValor(){
         PapelMoeda x = new PapelMoeda(0,0);
         assertEquals(0,x.getValor());
+    }
+    @Test
+    public void getQuantidade(){
+        PapelMoeda x = new PapelMoeda(0,0);
+        assertEquals(0,x.getQuantidade());
     }
 }
