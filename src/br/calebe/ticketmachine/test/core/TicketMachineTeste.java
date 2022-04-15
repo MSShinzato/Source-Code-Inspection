12d2a9bab0a74a284d3e921d4bdad20fa2144d86
Kaike <kzuanetti53@gmail.com>
2022-04-15 20:08:27 -0300

    add TicketMachineTest

--- src/br/calebe/ticketmachine/test/core/TicketMachineTeste.java
+++ src/br/calebe/ticketmachine/test/core/TicketMachineTeste.java
@@ -3,6 +3,7 @@ package br.calebe.ticketmachine.test.core;
 
 import br.calebe.ticketmachine.core.PapelMoeda;
 import br.calebe.ticketmachine.core.TicketMachine;
+import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
 import br.calebe.ticketmachine.exception.SaldoInsuficienteException;
 
 import static org.junit.Assert.assertEquals;
@@ -12,6 +13,19 @@ import org.junit.Test;
 public class TicketMachineTeste {
     
     
+    @Test
+    public void getSaldo(){
+        TicketMachine x = new TicketMachine(0);
+        assertEquals(0, x.getSaldo());
+    }
+
+    @Test
+    public void inserir() throws PapelMoedaInvalidaException{
+        TicketMachine x = new TicketMachine(0);
+        x.inserir(5);
+        assertEquals(5,x.getSaldo());
+    }
+
     @Test
     public void imprimir() throws SaldoInsuficienteException{
         TicketMachine x = new TicketMachine(0);
